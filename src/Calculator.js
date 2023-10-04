3d9c50b5ae7e22e2653bf6271436716a18fe2da0
Tejas VM <veeragantimanjunathtejas@gmail.com>
2023-10-04 13:42:07 -0400

    Updated the look and feel

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, Typography, Container, Box, Slider, Grid } from '@mui/material';
+import { TextField, Button, Typography, Container, Box, Slider, Grid, Card } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DatePicker } from '@mui/x-date-pickers';
@@ -53,123 +53,126 @@ function Calculator() {
 
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <Container component="main" maxWidth="sm" sx={{ mt: 5, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Typography component="h1" variant="h5">
-                    Pay Calculator
-                </Typography>
-                <Box component="form" width="100%" mt={2}>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        id="hourlyRate"
-                        label="Hourly Rate"
-                        value={hourlyRate}
-                        onChange={(e) => setHourlyRate(e.target.value)}
-                    />
-                    <Typography color="error" variant="body2">{payError}</Typography>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        type="number"
-                        id="weeklyHours"
-                        label="Weekly Hours"
-                        value={weeklyHours}
-                        onChange={(e) => {
-                            const value = parseInt(e.target.value, 10);
-                            if (value >= 1 && value <= 40) {
-                                setWeeklyHours(value);
-                            }
-                        }}
-                        helperText="Enter weekly hours (between 1 and 40)"
-                    />
-                    <Typography gutterBottom>Deduction Percentage: {deductionPercentage}%</Typography>
-                    <Slider
-                        value={deductionPercentage}
-                        onChange={(event, newValue) => setDeductionPercentage(newValue)}
-                        step={1}
-                        marks
-                        min={0}
-                        max={100}
-                        valueLabelDisplay="auto"
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        id="weeks"
-                        label="Weeks (default is 4)"
-                        name="weeks"
-                        value={weeks}
-                        onChange={(e) => setWeeks(e.target.value)}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        id="months"
-                        label="Months (default is 12)"
-                        name="months"
-                        value={months}
-                        onChange={(e) => setMonths(e.target.value)}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        id="years"
-                        label="Years (default is 1)"
-                        name="years"
-                        value={years}
-                        onChange={(e) => setYears(e.target.value)}
-                    />
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} sm={6}>
-                            <DatePicker
-                                label="Start Date"
-                                value={startDate}
-                                onChange={(newValue) => setStartDate(newValue)}
-                                textField={<TextField variant="outlined" margin="normal" fullWidth />}
-                            />
+            <Container component="main" maxWidth="md">
+                <Card square={false} elevation={24} sx={{ padding: 3, margin: "1em 0" }}>
+                    <Typography variant="h4" align="center" gutterBottom>Pay Calculator</Typography>
+                    <Box component="form" width="100%" mt={2}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="hourlyRate"
+                            label="Hourly Rate"
+                            value={hourlyRate}
+                            onChange={(e) => setHourlyRate(e.target.value)}
+                        />
+                        <Typography color="error" variant="body2">{payError}</Typography>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            type="number"
+                            id="weeklyHours"
+                            label="Weekly Hours"
+                            value={weeklyHours}
+                            onChange={(e) => {
+                                const value = parseInt(e.target.value, 10);
+                                if (value >= 1 && value <= 40) {
+                                    setWeeklyHours(value);
+                                }
+                            }}
+                            helperText="Enter weekly hours (between 1 and 40)"
+                        />
+                        <Typography gutterBottom>Deduction Percentage: {deductionPercentage}%</Typography>
+                        <Slider
+                            value={deductionPercentage}
+                            onChange={(event, newValue) => setDeductionPercentage(newValue)}
+                            step={1}
+                            marks
+                            min={0}
+                            max={100}
+                            valueLabelDisplay="auto"
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="weeks"
+                            label="Weeks (default is 4)"
+                            name="weeks"
+                            value={weeks}
+                            onChange={(e) => setWeeks(e.target.value)}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="months"
+                            label="Months (default is 12)"
+                            name="months"
+                            value={months}
+                            onChange={(e) => setMonths(e.target.value)}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="years"
+                            label="Years (default is 1)"
+                            name="years"
+                            value={years}
+                            onChange={(e) => setYears(e.target.value)}
+                        />
+                        <Grid container spacing={3}>
+                            <Grid item xs={12} sm={6}>
+                                <DatePicker
+                                    label="Start Date"
+                                    value={startDate}
+                                    onChange={(newValue) => setStartDate(newValue)}
+                                    textField={<TextField variant="outlined" margin="normal" fullWidth />}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <DatePicker
+                                    label="End Date"
+                                    value={endDate}
+                                    onChange={(newValue) => {
+                                        if (newValue > startDate) {
+                                            setEndDate(newValue);
+                                            setDateError("");
+                                        } else {
+                                            setDateError("End date should be greater than the start date.");
+                                            setEndDate(null);
+                                        }
+                                    }}
+                                    textField={<TextField variant="outlined" margin="normal" fullWidth />}
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <DatePicker
-                                label="End Date"
-                                value={endDate}
-                                onChange={(newValue) => {
-                                    if (newValue > startDate) {
-                                        setEndDate(newValue);
-                                        setDateError("");
-                                    } else {
-                                        setDateError("End date should be greater than the start date.");
-                                        setEndDate(null);
-                                    }
-                                }}
-                                textField={<TextField variant="outlined" margin="normal" fullWidth />}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Typography color="error" variant="body2">{dateError}</Typography>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        sx={{ mt: 3, mb: 2 }}
-                        onClick={calculatePay}
-                    >
-                        Calculate
-                    </Button>
-                    {pay && (
-                        <Box mt={2}>
-                            <Typography variant="body1">Daily Pay: ${pay.dailyPay.toFixed(2)}</Typography>
-                            <Typography variant="body1">Weekly Pay: ${pay.weeklyPay.toFixed(2)}</Typography>
-                            <Typography variant="body1">Monthly Pay: ${pay.monthlyPay.toFixed(2)}</Typography>
-                            <Typography variant="body1">Yearly Pay: ${pay.yearlyPay.toFixed(2)}</Typography>
-                            {days && <Typography variant="body1">The pay from {startDate.toLocaleDateString()} to {endDate.toLocaleDateString()} ({days} days) is ${(days * pay.dailyPay).toFixed(2)}</Typography>}
-                        </Box>
-                    )}
-                </Box>
+                        <Typography color="error" variant="body2">{dateError}</Typography>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={calculatePay}>
+                            Calculate
+                        </Button>
+
+                        <Typography variant="h6" gutterBottom>Calculated Pay:</Typography>
+                        <Card square={false} elevation={24} sx={{ padding: 3, margin: "1em 0" }}>
+                            {pay && (
+                                <Box mt={2}>
+                                    <Typography variant="body1">Daily Pay: ${pay.dailyPay.toFixed(2)}</Typography>
+                                    <Typography variant="body1">Weekly Pay: ${pay.weeklyPay.toFixed(2)}</Typography>
+                                    <Typography variant="body1">Monthly Pay: ${pay.monthlyPay.toFixed(2)}</Typography>
+                                    <Typography variant="body1">Yearly Pay: ${pay.yearlyPay.toFixed(2)}</Typography>
+                                    {days && <Typography variant="body1">The pay from {startDate.toLocaleDateString()} to {endDate.toLocaleDateString()} ({days} days) is ${(days * pay.dailyPay).toFixed(2)}</Typography>}
+                                </Box>
+                            )}
+                        </Card>
+                    </Box>
+                </Card>
             </Container>
         </LocalizationProvider>
     );
